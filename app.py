f7f9f0c9fa02e3bf0a56e5c81273f28aa3ed9ec0
as1238 <adriana.sierra@duek.edu>
2022-02-04 03:56:16 -0500

    test again

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from dash import *
 from testapp import *
 
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
+    html.Div(dcc.Input(id='input-box', type='text')),
+    html.Button('Submit', id='button'),
+    html.Div(id='output-container-button',
+             children='Please submit a value'),
     html.Img(src="assets/FX.jpg")
 ])
 
