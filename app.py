929156dd79c09d6629d0520bf93cf70670a9aee4
as1238 <adriana.sierra@duek.edu>
2022-02-04 04:12:20 -0500

    testing another image!

--- app.py
+++ app.py
@@ -10,15 +10,17 @@ app.layout = html.Div([
     html.Button('Submit', id='button'),
     html.Div(id='output-container-button',
              children='Please submit a value'),
-    html.Img(src="assets/FX.jpg")
+    html.Img(src="assets/image.jpg")
 ])
 
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    return 'Output: {}'.format(input_value)
+
+
 if __name__ == '__main__':
-    #app.run_server(debug=True)
+    # app.run_server(debug=True)
     app.run_server(host='localhost', port=3000, debug=True)
-
-
-
-
-
-
